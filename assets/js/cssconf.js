815c58c5a5250adcf9a7490496a88f6fdd1cb770
Robert Fleischmann <robert.fleischmann@akqa.com>
2015-04-18 20:10:01 +0100

    added slider for speaker

--- assets/js/cssconf.js
+++ assets/js/cssconf.js
@@ -1,5 +1,9 @@
+import {FastClick} from 'fastclick'
 import ToggleState from './ToggleState'
+import slider from './slider/slider.js'
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
+  FastClick.attach(document.body);
+  slider();
   let navToggle = new ToggleState('.nav-toggle', '.nav-main ul');
 }, false);
