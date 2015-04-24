19237bf0b078ad300fed54c63f433558c64fc624
Robert Fleischmann <robert.fleischmann@akqa.com>
2015-04-24 11:14:39 +0100

    update deploy script to handle multiple environments

--- deploy.js
+++ deploy.js
@@ -4,24 +4,41 @@ var Rsync = require('rsync');
 var config = require('./config.deployment.json');
 var sourceDir = path.join(__dirname, 'dest');
 
-if (config.destination) {
+var environment = process.argv[2];
 
-  var rsync = Rsync.build({
-    source:      sourceDir,
-    destination: config.destination,
-    exclude:     ['.git', '*.js.map', '*.css.map'],
-    flags:       'avz',
-    shell:       'ssh'
-  });
-
-  rsync.execute(function(error, stdout, stderr) {
-    if (error) {
-      console.log('Error:');
-      console.log(error, stderr);
-    } else {
-      console.log('Deployment successful!');
-    }
-  });
-} else {
-  console.log('No desitnation found in config file');
+if (!environment) {
+  console.log('No deployment environment selected.');
+  console.log('  Available options "staging", "live"');
+  console.log('  Example: ./deploy.js staging');
+  return;
 }
+
+if (!config[environment]) {
+  console.log('No deployment configuration found for "' + environment + '"');
+  return;
+}
+
+config = config[environment];
+
+if (!config.destination) {
+  console.log('No deployment desitnation found for environment "' + environment + '"');
+  return;
+}
+
+
+var rsync = Rsync.build({
+  source:      sourceDir,
+  destination: config.destination,
+  exclude:     ['.git', '*.js.map', '*.css.map'],
+  flags:       'avz',
+  shell:       'ssh'
+});
+
+rsync.execute(function(error, stdout, stderr) {
+  if (error) {
+    console.log('Error:');
+    console.log(error, stderr);
+  } else {
+    console.log('Deployment successful!');
+  }
+});
